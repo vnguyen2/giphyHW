ca786fb3ef97c85c31842951250eb9c2e5df3bf2
Vincent Nguyen <vnguyen2@gmail.com>
2016-09-09 19:01:56 -0500

    button array function working. display gifs is not working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,13 +3,37 @@ var animalList= ['chickens', 'dogs', 'bears'];
 var key = 'dc6zaTOxFJmzC';
 
 
-//need to create data for animals in buttons
-//grab input to create button from submit button
+//create buttons from the animalList array
+function renderButtons(){ 
 
+    $('#animalButtons').empty();
 
+    for (var i = 0; i < animalList.length; i++){
+
+        var a = $('<button>') 
+        a.addClass('animal');
+        a.attr('data-name', animalList[i]);
+        a.text(animalList[i]); 
+        $('#animalButtons').append(a); 
+    }
+}
+
+//adding additional animals to array
+$('#addAnimal').on('click', function(){
+
+    var animal = $('#animal-input').val().trim();
+
+    animalList.push(animal);
+        
+    renderButtons();
+
+    $('#animal-input').val("");
+
+    return false;
+})
 
 //on click to grab data from button and display the gif
-$('button').on('click', function() {
+$('.animal').on('click', function() {
         var animal = $(this).data('animal');
         var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=dc6zaTOxFJmzC&limit=10";
 
@@ -22,7 +46,6 @@ $('button').on('click', function() {
                 console.log(response)
 
                 var results = response.data;
-                //--------------------------------
 
                 for (var i = 0; i < results.length; i++) {
 
@@ -32,6 +55,8 @@ $('button').on('click', function() {
 
                     var animalImage = $('<img>');
                     animalImage.attr('src', results[i].images.fixed_height.url);
+                    animalImage.attr('data-state', 'still');
+                    animalImage.addClass('animalGif');
 
                     animalDiv.append(p);
                     animalDiv.append(animalImage);
@@ -41,4 +66,22 @@ $('button').on('click', function() {
                 }
 
             });
-    });
+});
+
+// $(document).on('click', '.animal', displayAnimalGif);
+
+//clicking image if still will display the animated gif and vice versa
+$('.animalGif').on('click', function(){
+
+	var state = $(this).attr('data-state'); 
+
+	if (state == 'still'){
+                $(this).attr('src', $(this).data('animate'));
+                $(this).attr('data-state', 'animate');
+            }else{
+                $(this).attr('src', $(this).data('still'));
+                $(this).attr('data-state', 'still');
+            }
+});
+
+renderButtons();
